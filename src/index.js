007f12cc51fb724295413ff84c6f32862b2d0608
n.badiullin <n.badiullin@transtelematica.ru>
2019-06-26 01:47:18 +0400

    very basic implementation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
+import 'flexboxgrid2';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Story from './components/Story';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Story />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
